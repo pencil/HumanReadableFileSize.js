ab344faf46c158acc36b953e4da453f5025dfa82
Nils Caspar <caspar@thinkclever.ch>
2012-02-08 23:13:44 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,16 @@
+HumanReadableFileSize.js
+========================
+A jQuery plugin to display file sizes in a human-readable format.
+
+Installation
+------------
+Include the JavaScript file in the header section of your HTML page *after* jQuery:
+    <script src="https://raw.github.com/pencil/HumanReadableFileSize.js/master/fileSize.js" type="text/javascript"></script>
+
+Usage Example
+-------------
+    alert($.fileSize(50000)); // 50 KB
+    alert($.fileSize(50000, 2, true)); // 48.83 KiB
+    alert($.fileSize(1843875982)); // 1.84 GB
+    alert($.fileSize(24886424, 1, true)); // 23.7 MiB
+    alert($.fileSize(445)); // 445 B
---
+++ fileSize.js
@@ -0,0 +1,20 @@
+(function($, undefined){
+	$.fileSize = (function() {
+		var _prefixesSI = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'],
+			_prefixesIEC = ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi', 'Yi'];
+		return (function(fileSize, precision, useIECPrefixes) {
+			var prefixes = useIECPrefixes ? _prefixesIEC : _prefixesSI,
+				base = useIECPrefixes ? 1024 : 1000,
+				index = (fileSize == 0 ? 0 : Math.floor(Math.log(fileSize)/Math.log(base)));
+			if(precision === undefined) {
+				precision = 2;
+			}
+			if(index != 0) {
+				var pow = Math.pow(10, precision);
+				fileSize = fileSize/Math.pow(base, index);
+				fileSize = Math.round(fileSize * Math.pow(10, precision))/Math.pow(10, precision);
+			}
+			return fileSize + ' ' + prefixes[index] + 'B';
+		});
+	})();
+})(jQuery);
