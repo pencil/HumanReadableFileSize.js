e5f8c7d4af11244f08e0f1c8a6f58eb00a68d3da
Nils Caspar <caspar@thinkclever.ch>
2012-02-09 00:07:12 +0100

    JSLint compliant

--- jquery.filesize.js
+++ jquery.filesize.js
@@ -5,23 +5,21 @@
  * Copyright (c) 2012 "Pencil" Nils Caspar
  * Licensed under the MIT, GPL, and BSD licenses.
  */
-(function($, undefined){
-	$.fileSize = (function() {
-		var _prefixesSI = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'],
-			_prefixesIEC = ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi', 'Yi'];
-		return (function(fileSize, precision, useIECPrefixes) {
-			var prefixes = useIECPrefixes ? _prefixesIEC : _prefixesSI,
-				base = useIECPrefixes ? 1024 : 1000,
-				index = (fileSize == 0 ? 0 : Math.floor(Math.log(fileSize)/Math.log(base)));
-			if(precision === undefined) {
-				precision = 2;
-			}
-			if(index != 0) {
-				var pow = Math.pow(10, precision);
-				fileSize = fileSize/Math.pow(base, index);
-				fileSize = Math.round(fileSize * Math.pow(10, precision))/Math.pow(10, precision);
-			}
-			return fileSize + ' ' + prefixes[index] + 'B';
-		});
-	})();
-})(jQuery);
+(function ($, udef) {
+  "use strict";
+  $.fileSize = (function () {
+    var prefixesSI = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'],
+      prefixesIEC = ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi', 'Yi'];
+    return function (fileSize, precision, useIECPrefixes) {
+      var prefixes = useIECPrefixes ? prefixesIEC : prefixesSI,
+        base = useIECPrefixes ? 1024 : 1000,
+        powedPrecision = Math.pow(10, precision === udef ? 2 : precision),
+        index = (!fileSize ? 0 : Math.floor(Math.log(fileSize) / Math.log(base)));
+      if (index !== 0) {
+        fileSize = fileSize / Math.pow(base, index);
+        fileSize = Math.round(fileSize * powedPrecision) / powedPrecision;
+      }
+      return fileSize + ' ' + prefixes[index] + 'B';
+    };
+  }());
+}(jQuery));
